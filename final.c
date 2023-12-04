cdcbf286b000460f5e8ef7059dd597079073ebba
pjs1231 <127822638+pjs1231@users.noreply.github.com>
2023-12-04 14:17:13 +0900

    Update final.c

--- final.c
+++ final.c
@@ -1,12 +1,4 @@
-cd c:\
 
-cd C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\
-
-vcvars64.bat 
-
-vcvars64
-
-​
 
 #include <stdio.h>
 
@@ -43,10 +35,48 @@ int main(int argc, char *argv[]) {
 }
 
 2.컴파일 방법
-    컴파일 명령 실
-cd C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\
-cl copyfile.c
-    프로그램 실행
-copyfile source.txt target.txt
+    
+    mkdir anu 
+    cd anu
 
+    notepad mycp.c
+    
+    #include <stdio.h>
+
+​
+
+int main(int argc, char *argv[]) {
+
+    FILE *s, *t;
+
+    char c;
+
+​
+
+    s = fopen(argv[1], "r"); 
+
+    t = fopen(argv[2], "w");
+
+​
+
+    while ((c = fgetc(s)) != EOF)
+
+        fputc(c, t);
+
+​
+
+    fclose(s);
+
+    fclose(t);
+
+​
+
+    return 0;
+
+}   
+저장
+    cl mycp.c
+    mycp.exe f1.txt f2.txt
+    
+    
     자가체점:100점
